ce0f4ff9c8910e5a2b41b5f19092ec53849d29a9
markkoszykowski <markkoszykowski@gmail.com>
2021-12-13 13:34:10 -0500

    completed backend

--- backend/main.py
+++ backend/main.py
@@ -1,16 +1,20 @@
 from .checks import check_existence, check_bounds, get_radial_distance, get_midpoint
+from .models import users_database, User, Passwords, Attempts
+from flask_login import login_user, logout_user, current_user
 from flask import Blueprint, jsonify, send_file, request
 from werkzeug.utils import secure_filename
-from flask_login import current_user
+from random import sample
+import itertools
+import uuid
 import os
 
-PROJECT_DIR = os.path.dirname(os.path.realpath(__file__))
 IMAGES_DIR = "images"
+PROJECT_DIR = os.path.dirname(os.path.realpath(__file__))
 
 main = Blueprint("main", __name__)
 
-
-RADIAL_DISTANCES = [5, 10, 50]
+MINIMUM_POINTS = 6
+RADIAL_DISTANCES = [5, 10, 15, 20, 30]
 
 
 @main.route("/ping", methods=["GET"])
@@ -21,15 +25,19 @@ def ping():
 @main.route("/", methods=["GET"])
 def index():
     if current_user.is_authenticated:
-        return jsonify(F"Hello {current_user.username}"), 200
+        return jsonify(F"Hello {current_user.username}, Thanks for helping our study!"), 200
     else:
         return jsonify("Please try our ECE455 - Cybersecurity Final Project!"), 200
 
 
 @main.route("/get_image/<image>", methods=["GET"])
 def get_image(image):
-    file_path = check_existence(secure_filename(image))
-    return (send_file(file_path), 200) if file_path else (jsonify("Requested Image Does Not Exist"), 404)
+    file_path = None
+    try:
+        file_path = check_existence(secure_filename(image))
+    except OSError:
+        pass
+    return (send_file(file_path), 200) if file_path else (jsonify("Requested image does not exist"), 404)
 
 
 @main.route("/get_password_images", methods=["GET"])
@@ -39,7 +47,7 @@ def get_password_images():
 
 @main.route("/getR", methods=["GET"])
 def get_radial_distances():
-    return jsonify({"R": RADIAL_DISTANCES}), 200
+    return jsonify({"R": sample(RADIAL_DISTANCES, 2)}), 200
 
 
 """
@@ -64,11 +72,99 @@ def get_radial_distances():
 """
 @main.route("/signup", methods=["POST"])
 def signup():
-    username = request.form.get("username")
-    radial_distance = request.form.get("radial_distance")
-    password = request.form.get("password")
+    body = request.json
+    if body is None:
+        return jsonify("No request data"), 400
+    username = body.get("username", None)
+    radial_distance = body.get("radial_distance", None)
+    password = body.get("password", None)
 
-    return jsonify(), 200
+    if username is None or radial_distance is None or password is None:
+        return jsonify("Request is missing parameters"), 400
+
+    # Make sure R is an integer and one that the backend deems valid
+    try:
+        radial_distance = int(radial_distance)
+    except ValueError:
+        return jsonify("Provided radial distance not valid"), 400
+
+    if radial_distance not in RADIAL_DISTANCES:
+        return jsonify("Invalid radial distance requested"), 400
+
+    # Check if user has a password
+    user = User.query.filter_by(username=username).first()
+    if user:
+        radial_distance_password = Passwords.query.filter_by(id=user.id, r=radial_distance).first()
+        if radial_distance_password:
+            return jsonify("User already has a password for this radial distance"), 400
+
+    # Password formatting
+    password = password.split(";")
+    if len(password) != 2:
+        return jsonify("Incorrect format"), 400
+
+    for entry, string in enumerate(password):
+        points = string.split(",")
+        if len(points) < MINIMUM_POINTS:
+            return jsonify("Not enough points used"), 400
+
+        password[entry] = [tuple(point.split()) for point in points]
+        if not all(len(point) == 3 for point in password[entry]):
+            return jsonify("Incorrect format"), 400
+
+        try:
+            password[entry] = [(check_existence(secure_filename(point[0])), int(point[1]), int(point[2])) for point in password[entry]]
+            [check_bounds(*point) for point in password[entry]]
+        except ValueError:
+            return jsonify("Incorrect format"), 400
+        except OSError:
+            return jsonify("Image does not exist"), 400
+        except IndexError:
+            return jsonify("Image coordinates out of bounds"), 400
+
+    radial_distance_attempts = None
+    if user:
+        radial_distance_attempts = Attempts.query.filter_by(id=user.id, r=radial_distance).first()
+        radial_distance_attempts.attempts += 1
+    else:
+        current_ids = users_database.session.query(User.id).all()
+        new_id = uuid.uuid4().hex
+        while new_id in current_ids:
+            new_id = uuid.uuid4().hex
+
+        user = User(id=new_id, username=username)
+        users_database.session.add(user)
+        radial_distance_attempts = Attempts(id=new_id, r=radial_distance, attempts=1, successes=0)
+        users_database.session.add(radial_distance_attempts)
+
+    midpoint_password = ""
+    first = True
+
+    # Compare 2 attempts
+    for entry1, entry2 in itertools.zip_longest(*password, fillvalue=("", -1, -1)):
+        if not first:
+            midpoint_password += ", "
+        first = False
+
+        if entry1[0] != entry2[0]:
+            users_database.session.commit()
+            return jsonify("Password attempts are not the same"), 406
+
+        if get_radial_distance((entry1[1], entry1[2]), (entry2[1], entry2[2])) > radial_distance:
+            users_database.session.commit()
+            return jsonify("Password attempts are not the same"), 406
+
+        mid_x, mid_y = get_midpoint((entry1[1], entry1[2]), (entry2[1], entry2[2]))
+        midpoint_password += F"{os.path.basename(entry1[0])} {mid_x} {mid_y}"
+
+    radial_distance_attempts.successes += 1
+    radial_distance_password = Passwords(id=user.id, r=radial_distance, password=midpoint_password)
+    users_database.session.add(radial_distance_password)
+    users_database.session.commit()
+
+    login_user(user, remember=True)
+
+    return jsonify("Success!"), 201
 
 
 """
@@ -92,8 +188,79 @@ def signup():
 """
 @main.route("/login", methods=["POST"])
 def login():
-    username = request.form.get("username")
-    radial_distance = request.form.get("radial_distance")
-    password = request.form.get("password")
+    body = request.json
+    if body is None:
+        return jsonify("No request data"), 400
+    username = body.get("username", None)
+    radial_distance = body.get("radial_distance", None)
+    password = body.get("password", None)
+
+    if username is None or radial_distance is None or password is None:
+        return jsonify("Request is missing parameters"), 400
+
+    # Make sure R is an integer and one that the backend deems valid
+    try:
+        radial_distance = int(radial_distance)
+    except ValueError:
+        return jsonify("Provided radial distance not valid"), 400
+
+    if radial_distance not in RADIAL_DISTANCES:
+        return jsonify("Invalid radial distance requested"), 400
+
+    # Check if user has a password
+    user = User.query.filter_by(username=username).first()
+    radial_distance_password = None
+    if user:
+        radial_distance_password = Passwords.query.filter_by(id=user.id, r=radial_distance).first()
+        if not radial_distance_password:
+            return jsonify("User does not have password setup for provided radial distance"), 400
+    else:
+        return jsonify("User has not set up an account"), 400
+
+    # Password formatting
+    password = password.split(",")
+
+    password = [tuple(point.split()) for point in password]
+    if not all(len(point) == 3 for point in password):
+        return jsonify("Incorrect format"), 400
+
+    try:
+        password = [(check_existence(secure_filename(point[0])), int(point[1]), int(point[2])) for point in password]
+        [check_bounds(*point) for point in password]
+    except ValueError:
+        return jsonify("Incorrect format"), 400
+    except OSError:
+        return jsonify("Image does not exist"), 400
+    except IndexError:
+        return jsonify("Image coordinates out of bounds"), 400
+
+    radial_distance_attempts = Attempts.query.filter_by(id=user.id, r=radial_distance).first()
+    radial_distance_attempts.attempts += 1
+
+    actual_password = radial_distance_password.password.split(",")
+    actual_password = [tuple(point.split()) for point in actual_password]
+
+    for provided_point, password_point in itertools.zip_longest(password, actual_password, fillvalue=("", -1, -1)):
+        if os.path.basename(provided_point[0]) != password_point[0]:
+            users_database.session.commit()
+            return jsonify("Incorrect Password"), 401
+
+        if get_radial_distance((provided_point[1], provided_point[2]), (int(password_point[1]), int(password_point[2]))) > radial_distance:
+            users_database.session.commit()
+            return jsonify("Incorrect Password"), 401
+
+    radial_distance_attempts.successes += 1
+    users_database.session.commit()
+
+    login_user(user, remember=True)
 
     return jsonify(), 200
+
+
+@main.route("/logout", methods=["GET"])
+def logout():
+    if current_user.is_authenticated:
+        logout_user()
+        return jsonify("Successfully logged out"), 200
+    else:
+        return jsonify("User not logged in"), 401
--- backend/models.py
+++ backend/models.py
@@ -5,8 +5,8 @@ from .login import login_manager
 users_database = SQLAlchemy()
 
 
-class User(users_database.Model):
-    id = users_database.Column(users_database.Integer, primary_key=True)
+class User(UserMixin, users_database.Model):
+    id = users_database.Column(users_database.String(32), primary_key=True)
     username = users_database.Column(users_database.String(128), unique=True, nullable=False)
 
     @login_manager.user_loader
@@ -15,7 +15,13 @@ class User(users_database.Model):
 
 
 class Passwords(users_database.Model):
-    id = users_database.Column(users_database.Integer, users_database.ForeignKey("user.id", ondelete="CASCADE"), primary_key=True)
+    id = users_database.Column(users_database.String(32), users_database.ForeignKey("user.id", ondelete="CASCADE"), primary_key=True)
     r = users_database.Column(users_database.Integer, primary_key=True)
     password = users_database.Column(users_database.String(512), nullable=False)
-    signup_attempts = users_database.Column(users_database.Integer, nullable=False)
+
+
+class Attempts(users_database.Model):
+    id = users_database.Column(users_database.String(32), users_database.ForeignKey("user.id", ondelete="CASCADE"), primary_key=True)
+    r = users_database.Column(users_database.Integer, primary_key=True)
+    attempts = users_database.Column(users_database.Integer, nullable=False)
+    successes = users_database.Column(users_database.Integer, nullable=False)
